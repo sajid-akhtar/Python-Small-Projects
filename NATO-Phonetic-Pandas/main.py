a69bcff0a0098d10679c4a70b7eb83c0237df803
sajid-akhtar <sajid.akhtar9823@gmail.com>
2023-11-07 05:43:49 +0100

    Added exception handling

--- NATO-Phonetic-Pandas/main.py
+++ NATO-Phonetic-Pandas/main.py
@@ -7,7 +7,16 @@ nato_dict = {row.letter: row.code for (index, row) in csv_data.iterrows()}
 
 # print(nato_dict)
 
-name = input("Enter your name: ").upper()
-nato_list = [nato_dict[s] for s in name]
 
-print(nato_list)
+def generate_phonetic():
+    name = input("Enter your name: ").upper()
+    try:
+        nato_list = [nato_dict[s] for s in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_list)
+
+
+generate_phonetic()
